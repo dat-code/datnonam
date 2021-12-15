6d2506df1fdc0c864a0d32a734d7a59bdaf6964e
dat-code <pdat1207@gmail.com>
2021-12-15 09:32:52 +0700

    đã thay đổi

--- Javascrip/index.js
+++ Javascrip/index.js
@@ -1,8 +1,2 @@
-// nội dung fai js
-// var h1;
-// h1 = 'sơn đặng';
-// alert (h1);
-
-var fullName = 'Sơn Đặng';
-var age = 18;
-var workAt = 'F8';
+var Intro = "Xin chao";
+document.getElementById("Intro").innerHTML = Intro;
